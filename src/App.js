aacae11e81f7f0c73ee4cd6d925ddaf15b256506
Chelsea Bryant <chelseabryant@Chelseas-MacBook-Pro.local>
2022-09-21 23:43:59 -0600

    Review work and add icon on photos

--- src/App.js
+++ src/App.js
@@ -27,179 +27,194 @@ function App() {
       <div className="welcome-border">
         <div className="welcome">Welcome back, Chelsea!</div>
       </div>
-      <div className="shop-link-body">
-        <div className="shop-links">
-          <img
-            src="https://images.unsplash.com/photo-1604304194650-3ba3cfa752fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
-            className="circle-image"
-            alt=""
-          />
-          <div>dried flowers</div>
+      <div>
+        <div className="shop-link-body">
+          <div className="shop-links">
+            <img
+              src="https://images.unsplash.com/photo-1604304194650-3ba3cfa752fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
+              className="circle-image"
+              alt=""
+            />
+            <div>dried flowers</div>
+          </div>
+          <div className="shop-links">
+            <img
+              src="https://images.unsplash.com/photo-1499332251574-a76a01d733fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=833&q=80"
+              className="circle-image"
+              alt=""
+            />
+            <div>books</div>
+          </div>
+          <div className="shop-links">
+            <img
+              src="https://images.unsplash.com/photo-1556395676-1925d260d81b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+              className="circle-image"
+              alt=""
+            />
+            <div>wine glasses</div>
+          </div>
+          <div className="shop-links">
+            <img
+              src="https://images.unsplash.com/photo-1619021016992-07909c5b85d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+              className="circle-image"
+              alt=""
+            />
+            <div>water bottles</div>
+          </div>
+          <div className="shop-links">
+            <img
+              src="https://images.unsplash.com/photo-1618220252344-8ec99ec624b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+              className="circle-image"
+              alt=""
+            />
+            <div>mirrors</div>
+          </div>
+          <div className="shop-links">
+            <img
+              src="https://images.unsplash.com/photo-1590080876351-941da357bde6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+              className="circle-image"
+              alt=""
+            />
+            <div>baking utensils</div>
+          </div>
         </div>
-        <div className="shop-links">
-          <img
-            src="https://images.unsplash.com/photo-1499332251574-a76a01d733fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=833&q=80"
-            className="circle-image"
-            alt=""
-          />
-          <div>books</div>
-        </div>
-        <div className="shop-links">
-          <img
-            src="https://images.unsplash.com/photo-1556395676-1925d260d81b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-            className="circle-image"
-            alt=""
-          />
-          <div>wine glasses</div>
-        </div>
-        <div className="shop-links">
-          <img
-            src="https://images.unsplash.com/photo-1619021016992-07909c5b85d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-            className="circle-image"
-            alt=""
-          />
-          <div>water bottles</div>
-        </div>
-        <div className="shop-links">
-          <img
-            src="https://images.unsplash.com/photo-1618220252344-8ec99ec624b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-            className="circle-image"
-            alt=""
-          />
-          <div>mirrors</div>
-        </div>
-        <div className="shop-links">
-          <img
-            src="https://images.unsplash.com/photo-1590080876351-941da357bde6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-            className="circle-image"
-            alt=""
-          />
-          <div>baking utensils</div>
-        </div>
-      </div>
-      <div className="whitespace"></div>
+        <div className="whitespace"></div>
 
-      <div className="staggard-images">
-        <div className="columns">
+        <div className="staggard-images">
+          <div className="columns">
+            <div className="price-container">
+              <img
+                src="https://images.unsplash.com/photo-1533158307587-828f0a76ef46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+                className="photo-short"
+                alt=""
+              />
+              <div className="heart-icon">3</div>
+              <div className="price">$$$</div>
+            </div>
+            <div className="price-container">
+              <img
+                src="https://images.unsplash.com/photo-1518675219903-c682c4b16b1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                className="photo-tall"
+                alt=""
+              />
+              <div className="heart-icon">3</div>
+              <div className="price">$$$</div>
+            </div>
+          </div>
+          <div className="columns">
+            <div className="price-container">
+              <img
+                src="https://images.unsplash.com/photo-1556804335-2fa563e93aae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                className="photo-tall"
+                alt=""
+              />
+              <div className="heart-icon">3</div>
+              <div className="price">$$$</div>
+            </div>
+            <div className="price-container">
+              <img
+                src="https://images.unsplash.com/photo-1533713692156-f70938dc0d54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+                className="photo-short"
+                alt=""
+              />
+              <div className="heart-icon">3</div>
+              <div className="price">$$$</div>
+            </div>
+          </div>
+          <div className="columns">
+            <div className="price-container">
+              <img
+                src="https://images.unsplash.com/photo-1560114928-40f1f1eb26a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                className="photo-short"
+                alt=""
+              />
+              <div className="heart-icon">3</div>
+              <div className="price">$$$</div>
+            </div>
+            <div className="price-container">
+              <img
+                src="https://images.unsplash.com/photo-1511201173873-c327e63eb6c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                className="photo-tall"
+                alt=""
+              />
+              <div className="heart-icon">3</div>
+              <div className="price">$$$</div>
+            </div>
+          </div>
+          <div className="columns">
+            <div className="price-container">
+              <img
+                src="https://images.unsplash.com/photo-1523217582562-09d0def993a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                className="photo-tall"
+                alt=""
+              />
+              <div className="heart-icon">3</div>
+              <div className="price">$$$</div>
+            </div>
+            <div className="price-container">
+              <img
+                src="https://images.unsplash.com/photo-1498747946579-bde604cb8f44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+                className="photo-short"
+                alt=""
+              />
+              <div className="heart-icon">3</div>
+              <div className="price">$$$</div>
+            </div>
+          </div>
+        </div>
+        <div className="sponsor-space">
+          <p className="sponsor">Sponsored by Etsy sellers</p>
+          <p className="fun-fact">
+            Fun fact: behind every sponsored item there is an Etsy seller hoping
+            you'll check out their shop
+          </p>
+        </div>
+        <div className="bottom-row">
           <div className="price-container">
             <img
-              src="https://images.unsplash.com/photo-1533158307587-828f0a76ef46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
-              className="photo-short"
+              src="https://images.unsplash.com/photo-1538775185588-21b672fbe836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+              className="bottom-image"
               alt=""
             />
+            <div className="heart-icon">3</div>
             <div className="price">$$$</div>
           </div>
           <div className="price-container">
             <img
-              src="https://images.unsplash.com/photo-1518675219903-c682c4b16b1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-              className="photo-tall"
+              src="https://images.unsplash.com/photo-1531525645387-7f14be1bdbbd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+              className="bottom-image"
               alt=""
             />
-            <div className="price">$$$</div>
-          </div>
-        </div>
-        <div className="columns">
-          <div className="price-container">
-            <img
-              src="https://images.unsplash.com/photo-1556804335-2fa563e93aae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-              className="photo-tall"
-              alt=""
-            />
+            <div className="heart-icon">3</div>
             <div className="price">$$$</div>
           </div>
           <div className="price-container">
             <img
-              src="https://images.unsplash.com/photo-1533713692156-f70938dc0d54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
-              className="photo-short"
+              src="https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+              className="bottom-image"
               alt=""
             />
-            <div className="price">$$$</div>
-          </div>
-        </div>
-        <div className="columns">
-          <div className="price-container">
-            <img
-              src="https://images.unsplash.com/photo-1560114928-40f1f1eb26a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-              className="photo-short"
-              alt=""
-            />
+            <div className="heart-icon">3</div>
             <div className="price">$$$</div>
           </div>
           <div className="price-container">
             <img
-              src="https://images.unsplash.com/photo-1511201173873-c327e63eb6c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-              className="photo-tall"
+              src="https://images.unsplash.com/photo-1536098561742-ca998e48cbcc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=836&q=80"
+              className="bottom-image"
               alt=""
             />
-            <div className="price">$$$</div>
-          </div>
-        </div>
-        <div className="columns">
-          <div className="price-container">
-            <img
-              src="https://images.unsplash.com/photo-1523217582562-09d0def993a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-              className="photo-tall"
-              alt=""
-            />
+            <div className="heart-icon">3</div>
             <div className="price">$$$</div>
           </div>
           <div className="price-container">
             <img
-              src="https://images.unsplash.com/photo-1498747946579-bde604cb8f44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
-              className="photo-short"
+              src="https://images.unsplash.com/photo-1592244851292-3364b6c46c22?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+              className="bottom-image"
               alt=""
             />
+            <div className="heart-icon">3</div>
             <div className="price">$$$</div>
           </div>
-        </div>
-      </div>
-      <div className="sponsor-space">
-        <p className="sponsor">Sponsored by Etsy sellers</p>
-        <p className="fun-fact">
-          Fun fact: behind every sponsored item there is an Etsy seller hoping
-          you'll check out their shop
-        </p>
-      </div>
-      <div className="bottom-row">
-        <div className="price-container">
-          <img
-            src="https://images.unsplash.com/photo-1538775185588-21b672fbe836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-            className="bottom-image"
-            alt=""
-          />
-          <div className="price">$$$</div>
-        </div>
-        <div className="price-container">
-          <img
-            src="https://images.unsplash.com/photo-1531525645387-7f14be1bdbbd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-            className="bottom-image"
-            alt=""
-          />
-          <div className="price">$$$</div>
-        </div>
-        <div className="price-container">
-          <img
-            src="https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-            className="bottom-image"
-            alt=""
-          />
-          <div className="price">$$$</div>
-        </div>
-        <div className="price-container">
-          <img
-            src="https://images.unsplash.com/photo-1536098561742-ca998e48cbcc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=836&q=80"
-            className="bottom-image"
-            alt=""
-          />
-          <div className="price">$$$</div>
-        </div>
-        <div className="price-container">
-          <img
-            src="https://images.unsplash.com/photo-1592244851292-3364b6c46c22?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-            className="bottom-image"
-            alt=""
-          />
-          <div className="price">$$$</div>
         </div>
       </div>
     </div>
